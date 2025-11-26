cef30d4ad1be6dac2b52069c190b799e3761d637
Saif Kenani <saifkhaledkenani@gmail.com>
2025-11-26 21:07:09 +0300

    Added DFS algorithms  to find the solution path, print the number of nodes visited and generated, and print the path taken

---
+++ src/algorithim/BFS.java
@@ -0,0 +1,55 @@
+package algorithim;
+
+import game.GameState;
+import game.NextStates;
+import logic.Direction;
+
+import javax.swing.plaf.nimbus.State;
+import java.util.*;
+
+public class BFS {
+
+   private final Set<GameState> visited=new HashSet<>();
+    int numNodesVisited=0;
+    int numNodesGenerated=0;
+
+
+    public BFS(GameState gameState) {
+            BFSSearch(gameState);
+    }
+
+
+    public void  BFSSearch(GameState root) {
+        visited.add(root);
+        numNodesVisited++;
+        Queue<GameState> queue =new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+
+            GameState state = queue.poll();
+            System.out.println(state);
+            if (state.checkWining()) {
+                System.out.println(new Common().getDirection(state));
+                System.out.println("number of visited node is +"+ visited.size());// == numNodesVisited
+                // System.out.println("number of visited node is +"+ numNodesVisited);// == numNodesVisited
+                System.out.println("number of Generated node is +"+ numNodesGenerated);
+                return;
+            }
+            NextStates nextStates = state.getNextStates();
+            numNodesGenerated+=nextStates.getSuccessors().size();
+            for (Map.Entry<logic.Direction, GameState> entry : nextStates.getSuccessors().entrySet()) {
+                addToQueue(queue, state, entry.getValue());
+            }
+        }
+    }
+
+    private void addToQueue(Queue<GameState> queue, GameState parent,GameState state) {
+        if (!visited.contains(state)) {
+            numNodesVisited++;
+            state.setParent(parent);
+            visited.add(state);
+            queue.add(state);
+        }
+    }
+
+}
