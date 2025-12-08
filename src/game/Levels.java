ab352e6f3a852e74e2932192912c0f49e636ed46
Saif Kenani <saifkhaledkenani@gmail.com>
2025-12-08 21:17:20 +0300

    Update src/game/Levels.java

--- src/game/Levels.java
+++ src/game/Levels.java
@@ -1,11 +1,68 @@
 package game;
 import basicStructure.*;
+import logic.MakeState;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Levels {
-   private Board boardSolvableNonCentral = new Board(5, 5, (row, col) -> {
+
+    String boardPath1 = "src/levels/board1.txt";
+    String coloresPath1 = "src/levels/color1.txt";
+
+
+    String boardPath2 = "src/levels/board2.txt";
+    String coloresPath2 = "src/levels/color2.txt";
+
+
+
+    String boardPath3 = "src/levels/board3.txt";
+    String coloresPath3 = "src/levels/color3.txt";
+
+
+    String boardPath4 = "src/levels/board4.txt";
+    String coloresPath4 = "src/levels/color4.txt";
+
+
+    String boardPath5 = "src/levels/board5.txt";
+    String coloresPath5 = "src/levels/color5.txt";
+
+
+    GameState state1=null;
+    GameState state2=null;
+    GameState state3=null;
+    GameState state4=null;
+    GameState state5=null;
+
+    public GameState getState5() {
+        return state5;
+    }
+
+    public GameState getState4() {
+        return state4;
+    }
+
+    public GameState getState3() {
+        return state3;
+    }
+
+    public GameState getState2() {
+        return state2;
+    }
+
+    public GameState getState1() {
+        return state1;
+    }
+
+    public Levels() throws IOException {
+         state1=new MakeState().importFromFiles(boardPath1,coloresPath1);
+         state2=new MakeState().importFromFiles(boardPath2,coloresPath2);
+         state3=new MakeState().importFromFiles(boardPath3,coloresPath3);
+         state4=new MakeState().importFromFiles(boardPath4,coloresPath4);
+         state5=new MakeState().importFromFiles(boardPath5,coloresPath5);    }
+
+    private Board boardSolvableNonCentral = new Board(5, 5, (row, col) -> {
        Map<Location,Square> map=new HashMap<>();
        SpecialSquare specialSquare;
 
